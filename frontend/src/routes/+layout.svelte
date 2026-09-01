328bd8e1eab894c7c2b9fcacfc608f6b3dea9cf9
BobDeUncle <jessemansbridge@hotmail.com>
2026-09-01 17:37:02 +1000

    Add header and background-colour to layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import './layout.css';
+	import '@fontsource/raleway';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from './Header.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<Header />
 {@render children()}
+
+<style>
+	:global(body) {
+		background-color: var(--color-primary-500);
+	}
+</style>
