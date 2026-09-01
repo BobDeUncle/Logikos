27132371699a89cc30b2b4de6036f2f1a991c942
BobDeUncle <jessemansbridge@hotmail.com>
2026-09-01 17:46:33 +1000

    Create simple Home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,19 @@
 <script lang="ts">
+	import '@fontsource/birthstone';
 </script>
+
+<h1 class="font-title">Jesse Mansbridge</h1>
+<h2 class="font-navigation">is Logikos - Web Developer & Writer</h2>
+
+<style>
+	h1 {
+		font-size: 8rem;
+		text-align: center;
+		margin-top: 6rem;
+	}
+
+	h2 {
+		font-size: 1.8rem;
+		text-align: center;
+	}
+</style>
